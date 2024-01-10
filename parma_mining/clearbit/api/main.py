5878c6cf27d3bcc87f7cf85a4fd84d78a6a4202a
Ege Kocabas <48245934+egekocabas@users.noreply.github.com>
2024-01-10 01:33:13 +0100

    refactor: Support different log levels for different environments (#19)

--- parma_mining/clearbit/api/main.py
+++ parma_mining/clearbit/api/main.py
@@ -1,9 +1,18 @@
 """Main entrypoint for the API routes in of parma-analytics."""
 import logging
+import os
 
 from fastapi import FastAPI, status
 
-logging.basicConfig(level=logging.INFO)
+env = os.getenv("env", "local")
+
+if env == "prod":
+    logging.basicConfig(level=logging.INFO)
+elif env in ["staging", "local"]:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.warning(f"Unknown environment '{env}'. Defaulting to INFO level.")
+    logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
