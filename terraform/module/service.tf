35b4ed3f62967a6147b7b4c0754ed3bdf17301b1
Ege Kocabas <48245934+egekocabas@users.noreply.github.com>
2024-01-16 18:30:00 +0100

    Update service.tf

--- terraform/module/service.tf
+++ terraform/module/service.tf
@@ -43,6 +43,10 @@ resource "google_cloud_run_service" "parma_mining_clearbit_cloud_run" {
           name  = "PARMA_SHARED_SECRET_KEY"
           value = var.PARMA_SHARED_SECRET_KEY
         }
+        env {
+          name  = "DEPLOYMENT_ENV"
+          value = var.env
+        }
       }
     }
   }
